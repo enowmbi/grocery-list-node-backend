2d014b340b5f4dc3c8a1ec47d64f5101d8cb50b7
Enow Mbi <benowmbi@yahoo.com>
2022-04-13 14:47:28 +0100

    Add index action

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+const connectionUrl = "mongodb://localhost/test"
+
+mongoose.connect(connectionUrl, (error) => {
+    if (error) return console.log("An error occurred" + error.message)
+    console.log("Connected to MongoDB successfully")
+})
+
+const grocerySchema = mongoose.Schema({
+    name: { type: String, unique: true },
+    checked: { type: Boolean, default: false },
+})
+
+const Grocery = new mongoose.model('Grocery', grocerySchema)
+
+app.get("/groceries || /", async(request, response) => {
+    const groceries = await Grocery.find()
+    response.send(groceries)
+    response.end
+})
 
 
 app.listen(PORT, (error) => {
